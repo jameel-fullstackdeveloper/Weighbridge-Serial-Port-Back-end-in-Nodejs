063683e8fe792d0faf7ddd8ae00e13d274f024e8
jameel-fullstackdeveloper <jameel@jameelahmed.com.pk>
2019-08-05 11:34:25 -0700

    tested / working code

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+  app = express(),
+  server = require("http").Server(app),
+  io = require("socket.io")(server),
+  port = 8888;
+
+const index = require("./routes/index");
+
+//user server
+app.use(index);
+
+io.on("connection", onConnection);
+
+var connectedSocket = null;
+function onConnection(socket) {
+  connectedSocket = socket;
+}
+
+//Serial port to CMD
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+const usbport = new SerialPort("COM1", { baudRate: 4800 });
+const parser = usbport.pipe(new Readline());
+parser.on("data", function(data) {
+  io.emit("data", { data: data });
+});
+
+//Server start
+server.listen(port, () => console.log("Listening to requests on port" + port));
